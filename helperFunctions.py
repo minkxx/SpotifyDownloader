2727163aee0c843104b2fa09334baa35c796016b
minkxx <aryuokk@gmail.com>
2024-01-13 18:33:52 +0530

    update : set cover art

--- helperFunctions.py
+++ helperFunctions.py
@@ -1,5 +1,8 @@
 import shutil
+import requests
 from moviepy.editor import *
+import eyed3
+from eyed3.id3.frames import ImageFrame
 
 def MP4ToMP3(mp4_path:str):
     mp3_path = mp4_path.split(".")[0] + ".mp3"
@@ -7,7 +10,22 @@ def MP4ToMP3(mp4_path:str):
     file.write_audiofile(mp3_path)
     file.close()
     os.remove(mp4_path)
+    return mp3_path
 
 def zip(zip_name:str, dir_path:str):
     path = shutil.make_archive(zip_name, "zip", dir_path)
     return path.split("\\")[-1]
+
+def setCoverArt(track_name:str, img_url:str, mp3_path:str):
+    img_data = requests.get(img_url).content
+    with open(f"imgs/{track_name}.png", 'wb') as f:
+        f.write(img_data)
+
+    img_path = f"imgs/{track_name}.png"
+
+    audiofile = eyed3.load(mp3_path)
+    if (audiofile.tag == None):
+        audiofile.initTag()
+    audiofile.tag.images.set(ImageFrame.FRONT_COVER, open(img_path,'rb').read(), 'image/png')
+    audiofile.tag.save()
+    os.remove(img_path)
--- spotifyDownloader.py
+++ spotifyDownloader.py
@@ -1,23 +1,27 @@
 from pySpotify import spotiSearch
 from pyYtDl import songDl
-from helperFunctions import MP4ToMP3, zip
+from helperFunctions import MP4ToMP3, setCoverArt, zip
 
 import config
 
 client = spotiSearch(config.SPOTIFY_CLIENT_ID, config.SPOTIFY_CLIENT_SECRET)
 
 url = input("Spotify Url : ")
+
 if ("track" in (url.split("/"))) and (client.validUrl(url)):
     song = client.getTrack(url)
     path = songDl(song[0], "songs/")
-    MP4ToMP3(path)
+    mp3_path = MP4ToMP3(path)
+    setCoverArt(song[0], song[1], mp3_path)
+
 elif ("album" in (url.split("/"))) and (client.validUrl(url)):
     album = client.getAlbum(url)
     dir_path =  f"songs/{album[0]}/"
     for i in album[1]:
         path = songDl(i, dir_path)
-        MP4ToMP3(path)
+        mp3_path = MP4ToMP3(path)
     zip(album[0], dir_path)
+
 elif ("playlist" in (url.split("/")))and (client.validUrl(url)):
     playlist = client.getPlaylist(url)
     dir_path =  f"songs/{playlist[0]}/"
