f287eb6ef0e2c358d74d1a22aded52783089e9a1
minkxx <aryuokk@gmail.com>
2024-01-13 11:38:33 +0530

    update : gui

--- gui.py
+++ gui.py
@@ -8,8 +8,10 @@ import config
 
 client = spotiSearch(config.SPOTIFY_CLIENT_ID, config.SPOTIFY_CLIENT_SECRET)
 
+global user_url
+
 def download():
-    url = url.get()
+    url = user_url.get()
     if ("track" in (url.split("/"))) and (client.validUrl(url)):
         songName = client.getTrack(url)
         path = songDl(songName, "songs/")
@@ -29,10 +31,14 @@ def download():
             MP4ToMP3(path)
         playlistSongs.clear()
     else:
-        print("Not a spotify link!")
+        output.set("Not a Spotify Link")
+        pass
 
 root = tk.Tk()
 root.title("Spotify Downloader")
+
+user_url = tk.StringVar()
+output = tk.StringVar()
 
 frame = ttk.Frame(root)
 
@@ -43,10 +49,11 @@ root.rowconfigure(0, weight=1)
 title = ttk.Label(frame, text="Spotify Downloader", font=("Microsoft JhengHei UI", 20), foreground="#32A852")
 title.grid(column=3,row=1)
 
-url = tk.StringVar()
-url_entry = ttk.Entry(frame, width=7, textvariable=url)
+url_entry = ttk.Entry(frame, width=7, textvariable=user_url)
 url_entry.grid(column=3, row=3, sticky=("W", "E"))
 
 ttk.Button(frame, text="Download", command=download).grid(column=3, row=5, sticky="W")
+
+output_label = ttk.Label(frame, text=output, font=("Microsoft JhengHei UI", 15)).grid(column=3, row=7)
 
 root.mainloop()
