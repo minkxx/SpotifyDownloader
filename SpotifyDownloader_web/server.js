2a8eb0c90f07026a7c9ba272382a454070fc29c7
Minkxx <114978467+minkxx@users.noreply.github.com>
2024-03-05 14:09:15 +0530

    Delete SpotifyDownloader_web directory

--- SpotifyDownloader_web/server.js
+++
@@ -1,8 +0,0 @@
-let downloadButton = document.getElementById("button")
-downloadButton.addEventListener("click", navigateToURL)
-
-function navigateToURL() {
-    let urlInput = document.getElementById('url');
-    let url = urlInput.value;
-    console.log(url)
-}
